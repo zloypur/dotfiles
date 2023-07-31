849f79876ac0268d76955324146f5514085dafdd
Alex Purii <zloypur@gmail.com>
2023-07-31 23:05:47 +0300

    Lsp: Enable autoselect for completion

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -75,7 +75,7 @@ local cmp_action = require('lsp-zero').cmp_action()
 cmp.setup({
     mapping = {
         -- `Enter` key to confirm completion
-        ['<CR>'] = cmp.mapping.confirm({ select = false }),
+        ['<CR>'] = cmp.mapping.confirm({ select = true }), -- false to disable autoselect
 
         -- Ctrl+Space to trigger completion menu
         ['<C-Space>'] = cmp.mapping.complete(),
