e1034f9b9a41c642cedf5c92153a5a916d7209e6
zloypur <zloypur@gmail.com>
2024-11-11 23:38:27 +0300

    Make nvim-tree a floating window

--- nvim/after/plugin/nvim-tree.lua
+++ nvim/after/plugin/nvim-tree.lua
@@ -5,9 +5,40 @@ vim.g.loaded_netrwPlugin = 1
 -- set termguicolors to enable highlight groups
 vim.opt.termguicolors = true
 
-require("nvim-tree").setup {
+local tree = require("nvim-tree")
+local api = require("nvim-tree.api")
+
+local HEIGHT_RATIO = 0.8
+local WIDTH_RATIO = 0.8
+
+tree.setup {
   view = {
     adaptive_size = true,
+    float = { -- this setup is taken from here https://github.com/nvim-tree/nvim-tree.lua/wiki/Recipes#center-a-floating-nvim-tree-window
+      enable = true,
+      open_win_config = function()
+        local screen_w = vim.opt.columns:get()
+        local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
+        local window_w = screen_w * WIDTH_RATIO
+        local window_h = screen_h * HEIGHT_RATIO
+        local window_w_int = math.floor(window_w)
+        local window_h_int = math.floor(window_h)
+        local center_x = (screen_w - window_w) / 2
+        local center_y = ((vim.opt.lines:get() - window_h) / 2)
+                         - vim.opt.cmdheight:get()
+        return {
+          border = 'rounded',
+          relative = 'editor',
+          row = center_y,
+          col = center_x,
+          width = window_w_int,
+          height = window_h_int,
+        }
+        end,
+    },
+    width = function()
+      return math.floor(vim.opt.columns:get() * WIDTH_RATIO)
+    end,
   },
   renderer = {
     group_empty = true,
@@ -17,8 +48,6 @@ require("nvim-tree").setup {
     git_ignored = false,
   },
 }
-local api = require("nvim-tree.api")
-
 
 -- custom mappings
 vim.keymap.set('n', '<leader>nn', api.tree.toggle, {})
