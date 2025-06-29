d9e95770f61814a8673f57ae273d694a5a5c239e
zloypur <zloypur@gmail.com>
2025-06-29 17:49:28 +0300

    Update tmux.conf

---
+++ nvim/lua/plugins/base/tmux.lua
@@ -0,0 +1,19 @@
+return {
+	{
+		"christoomey/vim-tmux-navigator",
+		cmd = {
+			"TmuxNavigateLeft",
+			"TmuxNavigateDown",
+			"TmuxNavigateUp",
+			"TmuxNavigateRight",
+			"TmuxNavigatePrevious",
+			"TmuxNavigatorProcessList",
+		},
+		keys = {
+			{ "<C-h>", "<cmd> TmuxNavigateLeft<cr>" },
+			{ "<C-j>", "<cmd> TmuxNavigateDown<cr>" },
+			{ "<C-k>", "<cmd> TmuxNavigateUp<cr>" },
+			{ "<C-l>", "<cmd> TmuxNavigateRight<cr>" },
+		},
+	},
+}
