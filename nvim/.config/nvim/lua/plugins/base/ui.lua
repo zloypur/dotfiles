08c4f07f55dd8988bebabba8f10f3f78fc2a1292
zloypur <zloypur@gmail.com>
2025-07-12 19:27:21 +0300

    Add another theme

--- nvim/.config/nvim/lua/plugins/base/ui.lua
+++ nvim/.config/nvim/lua/plugins/base/ui.lua
@@ -4,11 +4,24 @@ return {
 		lazy = false,
 		priority = 95,
 		config = function()
-            vim.opt.background = "dark" -- light, dark
-            vim.g.gruvbox_contrast_dark = "soft" -- soft, medium, hard
-            vim.g.gruvbox_contrast_light = "soft"
-            vim.g.gruvbox_italic = 1
-			vim.cmd.colorscheme("gruvbox")
+			vim.opt.background = "dark" -- light, dark
+			vim.g.gruvbox_contrast_dark = "soft" -- soft, medium, hard
+			vim.g.gruvbox_contrast_light = "soft"
+			vim.g.gruvbox_italic = 1
+			-- vim.cmd.colorscheme("gruvbox")
+		end,
+	},
+	{
+		"aliqyan-21/darkvoid.nvim",
+		lazy = false,
+		priority = 95,
+		config = function()
+			require("darkvoid").setup({
+				colors = {
+					string = "#f1f1f1",
+				},
+			})
+			vim.cmd.colorscheme("darkvoid")
 		end,
 	},
 }
