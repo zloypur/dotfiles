f637a685a738be3aadc5367f12f703d0bd3d864a
zloypur <zloypur@gmail.com>
2025-07-19 18:05:42 +0300

    Upgrade Mason to v2.0.0

--- nvim/.config/nvim/lua/plugins/base/mason.lua
+++ nvim/.config/nvim/lua/plugins/base/mason.lua
@@ -1,10 +1,12 @@
 return {
 	{
 		"mason-org/mason.nvim",
+		version = "v2.0.0",
 		opts = {},
 	},
 	{
 		"mason-org/mason-lspconfig.nvim",
+		version = "v2.0.0",
 		opts_extend = { "ensure_installed" },
 		opts = {
 			ensure_installed = {},
