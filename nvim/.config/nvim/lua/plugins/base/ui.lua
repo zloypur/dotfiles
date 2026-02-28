7399828b999dbb77323882baba65836565bde0fd
zloypur <zloypur@gmail.com>
2026-02-28 16:00:30 +0300

    update colorscheme

--- nvim/.config/nvim/lua/plugins/base/ui.lua
+++ nvim/.config/nvim/lua/plugins/base/ui.lua
@@ -1,27 +1,23 @@
 return {
 	{
+		"lunacookies/vim-colors-xcode",
+		lazy = false,
+		priority = 95, -- Ensure it loads first
+		config = function()
+			vim.opt.background = "light" -- light, dark
+			vim.cmd.colorscheme("xcodehc")
+		end,
+	},
+	{
 		"morhetz/gruvbox",
 		lazy = false,
 		priority = 95,
 		config = function()
-			vim.opt.background = "dark" -- light, dark
+			vim.opt.background = "light" -- light, dark
 			vim.g.gruvbox_contrast_dark = "soft" -- soft, medium, hard
 			vim.g.gruvbox_contrast_light = "soft"
 			vim.g.gruvbox_italic = 1
 			-- vim.cmd.colorscheme("gruvbox")
-		end,
-	},
-	{
-		"aliqyan-21/darkvoid.nvim",
-		lazy = false,
-		priority = 95,
-		config = function()
-			require("darkvoid").setup({
-				colors = {
-					string = "#f1f1f1",
-				},
-			})
-			vim.cmd.colorscheme("darkvoid")
 		end,
 	},
 }
