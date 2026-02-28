748ab5c4f5036798acc3b94817ac717b9b184f57
zloypur <zloypur@gmail.com>
2026-02-28 22:38:15 +0300

    update mason

--- nvim/.config/nvim/lua/plugins/base/mason.lua
+++ nvim/.config/nvim/lua/plugins/base/mason.lua
@@ -1,12 +1,12 @@
 return {
 	{
 		"mason-org/mason.nvim",
-		version = "v2.0.0",
+		version = "v2.2.1",
 		opts = {},
 	},
 	{
 		"mason-org/mason-lspconfig.nvim",
-		version = "v2.0.0",
+		version = "v2.2.1",
 		opts_extend = { "ensure_installed" },
 		opts = {
 			automatic_enable = false,
