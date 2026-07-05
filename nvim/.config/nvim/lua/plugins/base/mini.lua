34f77a35f253fdb96c9af7dc2823c506db5fc902
Александр Пурий <zloypur@gmail.com>
2026-07-05 23:02:42 +0300

    Update nvim config: update deps and add mini.ai, mini.splitjoin, mini.surround

---
+++ nvim/.config/nvim/lua/plugins/base/mini.lua
@@ -0,0 +1,33 @@
+return {
+	{
+		"nvim-mini/mini.ai",
+		version = "*",
+		opts = {},
+	},
+	{
+		"nvim-mini/mini.splitjoin",
+		version = "*",
+		opts = {
+			mappings = {
+				toggle = "<leader>cs",
+			},
+		},
+	},
+	{
+		"nvim-mini/mini.surround",
+		version = "*",
+		opts = {
+			mappings = {
+				add = "<leader>sa", -- Add surrounding in Normal and Visual modes
+				delete = "<leader>sd", -- Delete surrounding
+				find = "<leader>sf", -- Find surrounding (to the right)
+				find_left = "<leader>sF", -- Find surrounding (to the left)
+				highlight = "<leader>sh", -- Highlight surrounding
+				replace = "<leader>sr", -- Replace surrounding
+
+				suffix_last = "<leader>sl", -- Suffix to search with "prev" method
+				suffix_next = "<leader>sn", -- Suffix to search with "next" method
+			},
+		},
+	},
+}
